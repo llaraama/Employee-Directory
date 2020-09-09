5e1f3bcef625bd0630a5875ad2a8fc8cf1b3c631
Laura Lara <llaraama@uncc.edu>
2020-09-08 21:27:50 -0400

    Updates

--- employeeapp/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=50&nat=us";
-
-
-export default {
-  search: function() {
-    return axios.get(BASEURL);
-  }
-};
---
+++ precache-manifest.9dff150daa7a61c03795d5baf5d94c62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a66febcfc38451187a8e82eb1c9628a",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "9a7bee0265effac16b33",
+    "url": "/Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9fefb0cbf8ab93990cf2",
+    "url": "/Employee-Directory/static/css/main.b4a6ce4d.chunk.css"
+  },
+  {
+    "revision": "9a7bee0265effac16b33",
+    "url": "/Employee-Directory/static/js/2.d3a2a24a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.d3a2a24a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fefb0cbf8ab93990cf2",
+    "url": "/Employee-Directory/static/js/main.67c72b43.chunk.js"
+  },
+  {
+    "revision": "68b5aa4ddccbb2c46b8e",
+    "url": "/Employee-Directory/static/js/runtime-main.e96abd67.js"
+  }
+]);
